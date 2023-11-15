05674ccd8a1b83e1d865d190e04d5c14eeed241c
austin <143038805+aames2@users.noreply.github.com>
2023-11-14 21:23:24 -0500

    sprint 2

--- app.py
+++ app.py
@@ -1,23 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+posts = []
+
 
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
-@app.route("/forum", methods=["GET"])
-def forum():
-    return render_template("forum.html", forum=True)
 
 @app.route("/profile", methods=["GET"])
 def profile():
     return render_template("profile.html")
 
+
 @app.route("/login", methods=["GET"])
 def login():
     return render_template("login.html")
 
+
 @app.route("/new_login", methods=["GET"])
 def new_login():
     return render_template("new_login.html")
+
+
+@app.route("/make_post", methods=["GET", "POST"])
+def make_post():
+    if request.method == "POST":
+        title = request.form["discussionTitle"]
+        content = request.form["discussionContent"]
+        tags = request.form.getlist("tags")  # getting list of tags
+        # save the post data to a database
+        # add to posts list
+        posts.append({"title": title, "content": content, "tags": tags})
+        return redirect(url_for("forum"))
+    else:
+        return render_template("make_post.html")
+
+
+@app.route("/forum", methods=["GET"])
+def forum():
+    # Pass the list of posts to the forum template
+    return render_template("forum.html", posts=posts)
