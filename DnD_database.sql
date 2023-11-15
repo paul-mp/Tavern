e937572296785b506caa591afd90759828a11d30
Will Quinn <wquinn5262@gmail.com>
2023-11-14 21:20:07 -0500

    Committing initial DB setup

---
+++ DnD_database.sql
@@ -0,0 +1,42 @@
+CREATE TABLE IF NOT EXISTS Users(
+    username VARCHAR(255) UNIQUE NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE IF NOT EXISTS Characters(
+    character_id SERIAL NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    race VARCHAR(25) NOT NULL,
+    class VARCHAR(25) NOT NULL,
+    character_name VARCHAR(255) NOT NULL,
+    strength_attribute int NOT NULL,
+    dexterity_attribute int NOT NULL,
+    constitution_attribute int NOT NULL,
+    intelligence_attribute int NOT NULL,
+    wisdom_attribute int NOT NULL,
+    charisma_attribute int NOT NULL,
+    character_level int NOT NULL,
+    background VARCHAR(500) NOT NULL,
+    PRIMARY KEY (character_id),
+    FOREIGN KEY (username) REFERENCES Users(username)
+);
+
+CREATE TABLE IF NOT EXISTS Forum_posts(
+    username VARCHAR(255) NOT NULL,
+    post_id SERIAL NOT NULL, 
+    discussion_post VARCHAR(255) NOT NULL,
+    PRIMARY KEY (post_id),
+    FOREIGN KEY (username) REFERENCES Users(username)
+);
+
+CREATE TABLE IF NOT EXISTS Forum_comments(
+    username VARCHAR(255) NOT NULL,
+    post_id SERIAL NOT NULL,
+    discussion_comment VARCHAR(255) NOT NULL,
+    PRIMARY KEY (post_id, username),
+    FOREIGN KEY (username) REFERENCES Users(username),
+    FOREIGN KEY (post_id) REFERENCES Forum_posts(post_id)
+);
