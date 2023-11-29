c06cf163b6d536fd5c383b7cd96aa6d2a5833f73
Henry <hniworkline@gmail.com>
2023-11-28 20:31:00 -0500

    setting up src and making user_repository.py

---
+++ src/repositories/user_repository.py
@@ -0,0 +1,19 @@
+from models import User
+class UserRepository:
+
+    def get_all_post(self):
+        pass
+
+    def get_post_by_id(self, post_id):
+        pass
+
+    def create_post(self, title, author, tag):
+        pass
+
+    def get_profile(self, user_id):
+        pass
+
+    def ____(self,):
+        pass
+
+user_repository_singleton = UserRepository()
