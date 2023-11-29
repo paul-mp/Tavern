cf401a895af945e953a784b4abb0f4756fd1175f
austin <143038805+aames2@users.noreply.github.com>
2023-11-29 17:01:35 -0500

    Sprint 3 changes for my branch

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 import os
 from dotenv import load_dotenv
+from flask_migrate import Migrate
+
 
 load_dotenv()
 
-username = os.getenv('DB_USERNAME')
-password = os.getenv('DB_PASSWORD')
-host = os.getenv('DB_HOST')
-port = os.getenv('DB_PORT') 
-dbname = os.getenv('DB_NAME')
-secret_key = os.getenv('SECRET_KEY')
+username = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+dbname = os.getenv("DB_NAME")
+secret_key = os.getenv("SECRET_KEY")
 
 connection_string = f"postgresql://{username}:{password}@{host}:{port}/{dbname}"
 
@@ -22,8 +24,10 @@ app.secret_key = secret_key
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+migrate = Migrate(app, db)
 
-from models import User
+from models import User, Post
+
 
 @app.route("/", methods=["GET"])
 def index():
@@ -31,18 +35,20 @@ def index():
         return redirect("/post_login")
     return render_template("index.html")
 
+
 @app.route("/profile", methods=["GET"])
 def profile():
     if "username" not in session:
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
     return render_template("profile.html")
 
-@app.route('/signup', methods=['GET', 'POST'])
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    error = None 
-    if request.method == 'POST':
-        username = request.form.get('username')
-        raw_password = request.form.get('password')
+    error = None
+    if request.method == "POST":
+        username = request.form.get("username")
+        raw_password = request.form.get("password")
 
         if not username or not raw_password:
             error = "Invalid input."
@@ -52,59 +58,78 @@ def signup():
             if existing_user:
                 error = "Username already exists."
             else:
-                hashed_password = bcrypt.generate_password_hash(raw_password, 12).decode()
+                hashed_password = bcrypt.generate_password_hash(
+                    raw_password, 12
+                ).decode()
                 new_user = User(username, hashed_password)
                 db.session.add(new_user)
                 db.session.commit()
-                return redirect('/login')
-    
+                return redirect("/login")
+
     return render_template("sign_up.html", error=error)
 
-@app.route('/login', methods=["GET", "POST"])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     error_message = None
     if request.method == "POST":
-        username = request.form.get('username')
-        raw_password = request.form.get('password')
+        username = request.form.get("username")
+        raw_password = request.form.get("password")
         if not username or not raw_password:
-            error_message = 'Username and password are required.'
+            error_message = "Username and password are required."
         else:
             existing_user = User.query.filter_by(username=username).first()
-            if not existing_user or not bcrypt.check_password_hash(existing_user.password, raw_password):
-                error_message = 'Incorrect username or password.'
+            if not existing_user or not bcrypt.check_password_hash(
+                existing_user.password, raw_password
+            ):
+                error_message = "Incorrect username or password."
             else:
-                session['username'] = username
-                return redirect('/')
+                session["username"] = username
+                return redirect("/")
     return render_template("login.html", error=error_message)
+
 
 @app.route("/sign_up", methods=["GET"])
 def sign_up():
     return render_template("sign_up.html")
 
+
 @app.route("/user_profile", methods=["GET"])
 def user_profile():
     if "username" not in session:
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
     user = User.query.filter_by(username=session["username"]).first()
     if user:
-        join_date = user.creation_date.strftime('%m/%d/%Y')
-        return render_template("user_profile.html", username=session["username"], join_date=join_date)
+        join_date = user.creation_date.strftime("%m/%d/%Y")
+        return render_template(
+            "user_profile.html", username=session["username"], join_date=join_date
+        )
     else:
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
+
 
 @app.route("/make_post", methods=["GET", "POST"])
 def make_post():
     if "username" not in session:
-        return redirect(url_for('login'))
-    
+        return redirect(url_for("login"))
+
     if request.method == "POST":
         title = request.form["discussionTitle"]
         content = request.form["discussionContent"]
-        tags = request.form.getlist("tags")
-        posts.append({"title": title, "content": content, "tags": tags})
-        return redirect(url_for("forum"))
+        tags = ", ".join(request.form.getlist("tags"))  # Join tags into a string
+        user = User.query.filter_by(username=session["username"]).first()
+        if user:
+            new_post = Post(
+                title=title, content=content, tags=tags, user_id=user.user_id
+            )
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect(url_for("forum"))
+        else:
+            pass
     else:
         return render_template("make_post.html")
+
 
 @app.get("/post_login")
 def post_login():
@@ -112,16 +137,53 @@ def post_login():
         return abort(401)
     return render_template("post_login.html", username=session["username"])
 
+
 @app.route("/logout", methods=["POST"])
 def logout():
     session.pop("username", None)
     session.modified = True
     return redirect("/")
 
+
 @app.route("/forum", methods=["GET"])
 def forum():
-    posts = []
-    if "username" not in session:
-        return redirect(url_for('login'))
-    # Pass the list of posts to the forum template
+    posts = Post.query.all()
     return render_template("forum.html", posts=posts)
+
+
+@app.route("/delete_post/<int:post_id>", methods=["POST"])
+def delete_post(post_id):
+    if "username" not in session:
+        return redirect(url_for("login"))
+
+    post_to_delete = Post.query.get_or_404(post_id)
+    if session["username"] != post_to_delete.user.username:
+        return redirect(url_for("forum"))
+
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for("forum"))
+
+
+@app.route("/edit_post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    if "username" not in session:
+        return redirect(url_for("login"))
+
+    post_to_edit = Post.query.get_or_404(post_id)
+    if session["username"] != post_to_edit.user.username:
+        return redirect(url_for("forum"))
+
+    if request.method == "POST":
+        post_to_edit.title = request.form["title"]
+        post_to_edit.content = request.form["content"]
+        db.session.commit()
+        return redirect(url_for("forum"))
+    else:
+        return render_template("edit_post.html", post=post_to_edit)
+
+
+@app.route("/posts/<int:post_id>")
+def show_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post_detail.html", post=post)
