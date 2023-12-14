60f63da6ef01fb73dbb8768851fdf806346f09c6
Henry <hniworkline@gmail.com>
2023-12-14 15:18:17 -0500

    deleting unnesscary file

--- pytest/e2e/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from app import app
-
-
-@pytest.fixture(scope='module')
-def test_app():
-    return app.test_client()
--- pytest/e2e/test_all_forum_pages.py
+++
@@ -1,9 +0,0 @@
-from models import User
-from app import app
-
-def test_all_forum(test_app):
-    reponse = test_app.get('/forums')
-    
-    assert reponse.status_code == 200
-
-    assert b'Forum Discussions' in reponse.data
